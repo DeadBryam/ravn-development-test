ad29e58f8823c1e395b27a6c87fd502084e51ffd
sthbryan <bryamu.u@gmail.com>
2024-08-31 17:58:24 -0600

    feat: add colors and fonts

---
+++ src/config/tailwind/fontSize.ts
@@ -0,0 +1,67 @@
+const fontSize = {
+  'd-xl': [
+    '64px',
+    {
+      lineHeight: '88px',
+      letterSpacing: '1px',
+    },
+  ],
+  'd-l': [
+    '56px',
+    {
+      lineHeight: '72px',
+      letterSpacing: '1px',
+    },
+  ],
+  'd-m': [
+    '48px',
+    {
+      lineHeight: '56px',
+      letterSpacing: '1px',
+    },
+  ],
+  'd-s': [
+    '32px',
+    {
+      lineHeight: '48px',
+      letterSpacing: '1px',
+    },
+  ],
+  'd-xs': [
+    '24px',
+    {
+      lineHeight: '32px',
+      letterSpacing: '1px',
+    },
+  ],
+  'b-xl': [
+    '20px',
+    {
+      lineHeight: '32px',
+      letterSpacing: '0.75px',
+    },
+  ],
+  'b-l': [
+    '18px',
+    {
+      lineHeight: '32px',
+      letterSpacing: '0.75px',
+    },
+  ],
+  'b-m': [
+    '15px',
+    {
+      lineHeight: '24px',
+      letterSpacing: '0.75px',
+    },
+  ],
+  'b-s': [
+    '13px',
+    {
+      lineHeight: '22px',
+      letterSpacing: '0.75px',
+    },
+  ],
+};
+
+export { fontSize };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import { fontSize, colors } from './src/config/tailwind';
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize,
+      colors,
+    },
   },
   plugins: [],
-}
-
+};
