6f4573e89e0b962661f47a7f1e5b0e8bde536788
sthbryan <bryamu.u@gmail.com>
2024-09-01 16:37:09 -0600

    feat: add nav layout

---
+++ src/layouts/NavLayout.tsx
@@ -0,0 +1,31 @@
+import { IoMdMenu } from 'react-icons/io';
+import { PiSquaresFour } from 'react-icons/pi';
+import { Outlet } from 'react-router-dom';
+
+import { SideBar } from '@/components/SideBar';
+
+function NavLayout() {
+  const items = [
+    {
+      title: 'DASHBOARD',
+      icon: PiSquaresFour,
+      href: '/',
+    },
+    {
+      title: 'MY TASKS',
+      icon: IoMdMenu,
+      href: '/my-tasks',
+    },
+  ];
+
+  return (
+    <div id="nav-layout" className="p-8 flex flex-row gap-8 h-screen w-screen">
+      <SideBar items={items} className="min-w-[232px]" />
+      <section className="size-full flex-1 overflow-hidden">
+        <Outlet />
+      </section>
+    </div>
+  );
+}
+
+export { NavLayout };
