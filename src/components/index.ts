00a8b4061ef9cec53e58affffbc38700d4251540
sthbryan <bryamu.u@gmail.com>
2024-09-01 01:30:12 -0600

    feat: add SideBarItem component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ export * from './Avatar';
 export * from './Button';
 export * from './Card';
 export * from './PlatformTag';
+export * from './SideBarItem';
 export * from './SwitchButton';
 export * from './Tag';
