1632c67e8b6bdd5764cc5d65a9535b00f3dd3e4f
sthbryan <bryamu.u@gmail.com>
2024-08-31 19:41:24 -0600

    refactor: improve Button component styling and class handling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,26 @@
 import clsx from 'clsx';
+import { useMemo } from 'react';
 
 type ButtonProps = {
   children: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 function Button({ children, className, ...props }: ButtonProps) {
+  const containsBg = useMemo(() => {
+    return className?.includes('bg-');
+  }, [className]);
+
   return (
-    <button {...props} className={clsx('default-button', className)}>
+    <button
+      {...props}
+      className={clsx(
+        {
+          'default-button': !containsBg,
+          'transparent-button': containsBg,
+        },
+        className
+      )}
+    >
       {children}
     </button>
   );
