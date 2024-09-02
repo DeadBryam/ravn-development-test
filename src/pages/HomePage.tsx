88c940c0fe146e0bbc72d627d112227a282d1c40
sthbryan <bryamu.u@gmail.com>
2024-09-02 02:41:41 -0600

    feat: implement TaskModal in home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,13 +4,17 @@ import { IoMdMenu } from 'react-icons/io';
 import { PiSquaresFour } from 'react-icons/pi';
 
 import { SwitchButton, TopNavigationBar } from '@/components';
+import { TaskModal } from '@/components/TaskModal';
 import { TaskContainer } from '@/components/TasksContainer';
 
 function HomePage() {
   const [useGrid, setUseGrid] = useState<boolean>(true);
+  const [showModal, setShowModal] = useState<boolean>(false);
 
   const setGrid = useCallback(() => setUseGrid(true), []);
   const setList = useCallback(() => setUseGrid(false), []);
+  const openModal = useCallback(() => setShowModal(true), []);
+  const closeModal = useCallback(() => setShowModal(false), []);
 
   return (
     <div className="flex flex-col size-full">
@@ -24,11 +28,12 @@ function HomePage() {
             <PiSquaresFour size={24} />
           </SwitchButton>
         </div>
-        <SwitchButton className="!bg-primary-4">
+        <SwitchButton className="!bg-primary-4" onClick={openModal}>
           <FiPlus size={24} />
         </SwitchButton>
       </div>
       <TaskContainer className="flex-1" />
+      <TaskModal open={showModal} onClose={closeModal} />
     </div>
   );
 }
