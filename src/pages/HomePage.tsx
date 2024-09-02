95f21d5b42135243fa209dc23921c4a6354a9048
sthbryan <bryamu.u@gmail.com>
2024-09-01 20:13:24 -0600

    feat: complete home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,36 @@
+import { useCallback, useState } from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { IoMdMenu } from 'react-icons/io';
+import { PiSquaresFour } from 'react-icons/pi';
+
+import { SwitchButton, TopNavigationBar } from '@/components';
+import { TaskContainer } from '@/components/TasksContainer';
+
 function HomePage() {
-  return <></>;
+  const [useGrid, setUseGrid] = useState<boolean>(true);
+
+  const setGrid = useCallback(() => setUseGrid(true), []);
+  const setList = useCallback(() => setUseGrid(false), []);
+
+  return (
+    <div className="flex flex-col size-full">
+      <TopNavigationBar />
+      <div className="flex flex-row justify-between my-8 mb-4">
+        <div className="flex flex-row">
+          <SwitchButton active={!useGrid} onClick={setList}>
+            <IoMdMenu size={24} />
+          </SwitchButton>
+          <SwitchButton active={useGrid} onClick={setGrid}>
+            <PiSquaresFour size={24} />
+          </SwitchButton>
+        </div>
+        <SwitchButton className="!bg-primary-4">
+          <FiPlus size={24} />
+        </SwitchButton>
+      </div>
+      <TaskContainer className="flex-1" />
+    </div>
+  );
 }
 
 export default HomePage;
