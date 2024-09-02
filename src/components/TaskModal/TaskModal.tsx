7f2ad97fa6f97587426b91c33e72e773422bf06c
sthbryan <bryamu.u@gmail.com>
2024-09-02 13:38:27 -0600

    refactor: implement Modal in TaskModal component

--- src/components/TaskModal/TaskModal.tsx
+++ src/components/TaskModal/TaskModal.tsx
@@ -1,8 +1,6 @@
-import { Dialog, DialogPanel } from '@headlessui/react';
 import { nopeResolver } from '@hookform/resolvers/nope';
 import { useCallback, useMemo } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
-import { If, Then } from 'react-if';
 import { Form } from 'react-router-dom';
 
 import { CreateTaskInput, PointEstimate, Status, Task } from '@/__generated__/types';
@@ -10,9 +8,8 @@ import { useCreateTask, useUpdateTask } from '@/hooks';
 import { createTaskSchema } from '@/schemas';
 import { useTaskModalStore } from '@/stores';
 
-import { Button } from '../Button';
-import { ErrorBoundary } from '../ErrorBoundary';
 import { ErrorMessage } from '../Form';
+import { Modal } from '../Modal';
 import { AssigneeSelector } from './AssigneeSelector';
 import { DueDateSelector } from './DueDateSelector';
 import { EstimateSelector } from './EstimateSelector';
@@ -72,39 +69,22 @@ function TaskModal() {
   );
 
   return (
-    <Dialog open={open} onClose={() => {}} className="relative z-50">
-      <div className="fixed inset-0 flex w-screen items-center justify-center p-4 bg-neutral-4 bg-opacity-50">
-        <ErrorBoundary>
-          <FormProvider {...form}>
-            <Form>
-              <DialogPanel className="rounded-lg p-4 bg-neutral-3 shadow-md w-full gap-6 flex flex-col">
-                <div>
-                  <input type="text" placeholder="Task name" className="w-full p-2 h-8 rounded-md bg-neutral-3" {...form.register('name')} />
-                  <ErrorMessage name="name" />
-                </div>
-                <div className="flex flex-wrap items-start gap-4">
-                  <EstimateSelector name="pointEstimate" className="task-modal-field" />
-                  <AssigneeSelector name="assigneeId" className="task-modal-field" />
-                  <TagsSelector name="tags" className="task-modal-field" />
-                  <DueDateSelector name="dueDate" className="task-modal-field" />
-                </div>
-                <If condition={Boolean(form.formState.errors)}>
-                  <Then></Then>
-                </If>
-                <div className="flex flex-row gap-6 justify-end">
-                  <Button type="button" className="bg-transparent" onClick={close} disabled={loading}>
-                    Cancel
-                  </Button>
-                  <Button type="submit" isLoading={loading} onClick={form.handleSubmit(onSubmit)}>
-                    {isEditing ? 'Update' : 'Create'}
-                  </Button>
-                </div>
-              </DialogPanel>
-            </Form>
-          </FormProvider>
-        </ErrorBoundary>
-      </div>
-    </Dialog>
+    <Modal open={open} onCancel={close} onAccept={form.handleSubmit(onSubmit)} loading={loading} acceptText={isEditing ? 'Update' : 'Create'}>
+      <FormProvider {...form}>
+        <Form className="flex flex-col gap-4">
+          <div className="flex flex-col">
+            <input type="text" placeholder="Task name" className="w-full p-2 h-8 rounded-md bg-neutral-3" {...form.register('name')} />
+            <ErrorMessage name="name" />
+          </div>
+          <div className="flex flex-wrap items-start gap-4">
+            <EstimateSelector name="pointEstimate" className="task-modal-field" />
+            <AssigneeSelector name="assigneeId" className="task-modal-field" />
+            <TagsSelector name="tags" className="task-modal-field" />
+            <DueDateSelector name="dueDate" className="task-modal-field" />
+          </div>
+        </Form>
+      </FormProvider>
+    </Modal>
   );
 }
 
