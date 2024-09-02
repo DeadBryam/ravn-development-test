bc60df700c17ecb3840acca2c336b7b2eadb6b84
sthbryan <bryamu.u@gmail.com>
2024-09-02 13:38:07 -0600

    refactor: implement confirmation modal in TaskColumn

--- src/components/TasksContainer/TaskColumn.tsx
+++ src/components/TasksContainer/TaskColumn.tsx
@@ -1,11 +1,12 @@
 import clsx from 'clsx';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 import { Status, Task } from '@/__generated__/types';
 import { useDeleteTask } from '@/hooks';
 import { useTaskModalStore } from '@/stores';
 import { convertStatus } from '@/utils';
 
+import { ConfirmationModal } from '../ConfirmationModal';
 import { TaskCard } from '../TaskCard';
 
 type TaskColumnProps = {
@@ -15,24 +16,24 @@ type TaskColumnProps = {
 };
 
 function TaskColumn({ status, tasks = [], className }: TaskColumnProps) {
+  const [task, setTask] = useState<Task | null>(null);
   const [deleteTask] = useDeleteTask();
   const { openModal } = useTaskModalStore();
 
-  const onDelete = useCallback(
-    (task: Task) => {
-      deleteTask({ variables: { input: { id: task.id } }, optimisticResponse: { deleteTask: task } });
-    },
-    [deleteTask]
-  );
+  const onDelete = useCallback(() => {
+    deleteTask({ variables: { input: { id: task!.id } }, optimisticResponse: { deleteTask: task! } });
+    setTask(null);
+  }, [deleteTask, task]);
 
   return (
     <div key={status} className={clsx('flex flex-col gap-4', className)}>
+      <ConfirmationModal open={Boolean(task)} onCancel={setTask.bind(null, null)} onAccept={onDelete} />
       <h2 className="text-b-l font-semibold">
         {convertStatus(status)} ({tasks.length?.toString()?.padStart(2, '0')})
       </h2>
       <div className="tasks-column h-full">
         {tasks.map((task: Task) => (
-          <TaskCard key={task.id} task={task} onDelete={onDelete.bind(null, task)} onEdit={openModal.bind(null, task)} />
+          <TaskCard key={task.id} task={task} onDelete={setTask.bind(null, task)} onEdit={openModal.bind(null, task)} />
         ))}
       </div>
     </div>
