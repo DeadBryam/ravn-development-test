5a84ae3a9a0e394f801b6f7c8876940497af9811
sthbryan <bryamu.u@gmail.com>
2024-09-02 22:40:14 -0600

    refactor: update NavLayout responsive

--- src/layouts/NavLayout.tsx
+++ src/layouts/NavLayout.tsx
@@ -2,6 +2,7 @@ import { IoIosSettings } from 'react-icons/io';
 import { PiSquaresFour } from 'react-icons/pi';
 import { Outlet } from 'react-router-dom';
 
+import { BottomBar } from '@/components';
 import { SideBar } from '@/components/SideBar';
 
 function NavLayout() {
@@ -19,11 +20,14 @@ function NavLayout() {
   ];
 
   return (
-    <div id="nav-layout" className="p-8 flex flex-row gap-8 h-screen w-screen">
-      <SideBar items={items} className="min-w-[232px]" />
-      <section className="size-full flex-1 overflow-hidden">
-        <Outlet />
-      </section>
+    <div className="flex flex-col h-screen w-screen">
+      <div id="nav-layout" className="p-2 md:p-6 lg:p-8 flex flex-row gap-8 h-full w-full overflow-hidden">
+        <SideBar items={items} className="min-w-[232px] hidden pointer-events-none lg:block lg:pointer-events-auto" />
+        <section className="size-full flex-1 flex flex-col gap-4 h-full overflow-hidden">
+          <Outlet />
+        </section>
+      </div>
+      <BottomBar items={items} className="lg:hidden lg:poiner-events-none" />
     </div>
   );
 }
