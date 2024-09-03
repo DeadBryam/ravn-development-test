77cc89ee73af6e93b90d3d68973cda4a40b9aa25
sthbryan <bryamu.u@gmail.com>
2024-09-03 00:23:37 -0600

    refactor: configure test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'happy-dom',
+  },
   resolve: {
     alias: [
       {
