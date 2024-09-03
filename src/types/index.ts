bd730394d5574dec7528b873d54dbc7a065948e1
sthbryan <bryamu.u@gmail.com>
2024-09-03 00:13:18 -0600

    feat: create navIcon type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,2 @@
+export * from './navIcon';
 export * from './tasklist';
---
+++ src/types/navIcon.ts
@@ -0,0 +1,7 @@
+import { IconType } from 'react-icons';
+
+export type NavIcon = {
+  title: string;
+  icon: IconType;
+  href: string;
+};
